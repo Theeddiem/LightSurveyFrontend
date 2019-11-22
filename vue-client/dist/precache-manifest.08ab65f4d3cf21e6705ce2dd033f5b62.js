23da68654378829536dcf88024c4227735b22e57
Theeddiem <eddieknaz@gmail.com>
2019-11-22 20:04:09 +0200

    dssd

---
+++ vue-client/dist/precache-manifest.08ab65f4d3cf21e6705ce2dd033f5b62.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42e8dde89a214e063a4b",
+    "url": "/css/app.19015449.css"
+  },
+  {
+    "revision": "5ee0f2d1a1e6e14d8c4d8fa7f18c3326",
+    "url": "/fonts/luckiestguy.5ee0f2d1.ttf"
+  },
+  {
+    "revision": "060425cabf7163ccc29032fe5fc02500",
+    "url": "/img/peopleIcon.060425ca.svg"
+  },
+  {
+    "revision": "d064e66c326b08a86e52a8529143b6e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42e8dde89a214e063a4b",
+    "url": "/js/app.8b4b7d9c.js"
+  },
+  {
+    "revision": "4860bb2c80ef9f1d04eb",
+    "url": "/js/chunk-vendors.76a9e705.js"
+  },
+  {
+    "revision": "3c2fb072c5a3bde1c78bb601ecbed646",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- vue-client/dist/service-worker.js
+++ vue-client/dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.14e4660dd3398d2fd3778f82ea4a8128.js"
+  "/precache-manifest.08ab65f4d3cf21e6705ce2dd033f5b62.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue-client"});
