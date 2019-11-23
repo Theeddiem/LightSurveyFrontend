3cfb5f086309c78ded975e92dcb8b686351e8623
Theeddiem <eddieknaz@gmail.com>
2019-11-23 22:04:08 +0200

    ds

--- vue-client/src/App.vue
+++ vue-client/src/App.vue
@@ -20,7 +20,7 @@
 .App-container{
 
   display: grid;
-  grid-row-gap: 20px;
+  grid-gap: 20px;
   grid-template-columns: 1fr 2fr 1fr;
   /* grid-template-rows: 1fr 5fr 1fr; */
   text-align: center;
