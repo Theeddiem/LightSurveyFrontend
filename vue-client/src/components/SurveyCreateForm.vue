24d1a31c2747f4a4723dc321636dbc97b075dd61
theeddiem <eddieknaz@gmail.com>
2022-02-06 20:54:43 +0200

    dir-> auto left/right hebrew enligsh supp

--- vue-client/src/components/SurveyCreateForm.vue
+++ vue-client/src/components/SurveyCreateForm.vue
@@ -1,89 +1,83 @@
 <template>
   <div class="surveyCreate-container">
-       <input
-        class="input-group"
-        v-model.lazy="question"
-        type="text"
-        placeholder="Enter Question"
-      />
-      <OptionInput
-        @OnSetOption="setOption"
-        class="input-group"
-        v-for="(option, i) in options"
-        v-bind:key="i"
-        :index="i"
-      ></OptionInput>
-      <button class="button-group" @click="addOption">Add</button>
-      <button class="button-group" @click="submitPoll">Submit</button>
-     <pulse-loader :loading="loading" style="margin-top:10px"></pulse-loader>
+    <input
+      dir="auto"
+      class="input-group"
+      v-model.lazy="question"
+      type="text"
+      placeholder="Enter Question"
+    />
+    <OptionInput
+      @OnSetOption="setOption"
+      class="input-group"
+      v-for="(option, i) in options"
+      v-bind:key="i"
+      :index="i"
+    ></OptionInput>
+    <button class="button-group" @click="addOption">Add</button>
+    <button class="button-group" @click="submitPoll">Submit</button>
+    <pulse-loader :loading="loading" style="margin-top:10px"></pulse-loader>
   </div>
-
-  
 </template>
 
 <script>
-import Survey from '../SurveyClass'
-import IndicatorPopup from '../utilities'
+import Survey from "../SurveyClass";
+import IndicatorPopup from "../utilities";
 import Swal from "sweetalert2";
-import { postSurvey } from '../databaseManager'
-import OptionInput from './OptionInput.vue'
-import PulseLoader from 'vue-spinner/src/PulseLoader.vue'
+import { postSurvey } from "../databaseManager";
+import OptionInput from "./OptionInput.vue";
+import PulseLoader from "vue-spinner/src/PulseLoader.vue";
 
 export default {
+  components: { OptionInput, PulseLoader },
 
-    components: {OptionInput,  PulseLoader},
-
-  data: function () {
+  data: function() {
     return {
-      options : ['',''],
-      question: '',
-      loading: false,
-    }
+      options: ["", ""],
+      question: "",
+      loading: false
+    };
   },
 
   methods: {
- 
-    async submitPoll () {
-      const filterdOptions =  this.options.filter(
-        word => word !== ''
-      )
+    async submitPoll() {
+      const filterdOptions = this.options.filter(word => word !== "");
 
-      this.question = this.question.trim()
-      if (this.question === '') {
-        IndicatorPopup('Enter question first', 'warning')
-        return
+      this.question = this.question.trim();
+      if (this.question === "") {
+        IndicatorPopup("Enter question first", "warning");
+        return;
       }
 
-      if(this.question.length <  3)
-      {
-        IndicatorPopup('The question is too short', 'warning')
-        return
+      if (this.question.length < 3) {
+        IndicatorPopup("The question is too short", "warning");
+        return;
       }
-     
+
       if (filterdOptions.length < 2) {
-        IndicatorPopup('Enter at least 2 options', 'warning')
+        IndicatorPopup("Enter at least 2 options", "warning");
       } else {
-      this.loading = true
-      const currenySurvey  = await postSurvey(new Survey(this.question, filterdOptions))
-       this.loading = false
-      this.$router.push(`/survey/${currenySurvey.uuid}`)
+        this.loading = true;
+        const currenySurvey = await postSurvey(
+          new Survey(this.question, filterdOptions)
+        );
+        this.loading = false;
+        this.$router.push(`/survey/${currenySurvey.uuid}`);
       }
     },
 
-    setOption: function (option, index) {
-      if(option.trim()!='')
-        this.options[index] = option
+    setOption: function(option, index) {
+      if (option.trim() != "") this.options[index] = option;
     },
 
-    addOption: function () {
-      this.options.push('')
+    addOption: function() {
+      this.options.push("");
     }
   }
-}
+};
 </script>
 
 <style>
-
 .surveyCreate-container {
   padding: 15px;
   grid-column: 2;
@@ -95,7 +89,4 @@ export default {
   box-shadow: 0 2px 3px rgba(10, 10, 10, 0.1), 0 0 0 1px rgba(10, 10, 10, 0.1);
   max-width: 500px;
 }
-
-
-
 </style>
